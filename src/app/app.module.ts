2d4db1afdabfa90f98cc5d5d7fbf8d9ffd311b9d
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2022-02-23 17:23:40 -0300

    Adiciona header e footer na pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LivrosComponent } from './livros/livros.component';
 import { LivrosDetailsComponent } from './livros/livros-details/livros-details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, LivrosComponent, LivrosDetailsComponent],
+  declarations: [
+    AppComponent,
+    LivrosComponent,
+    LivrosDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
