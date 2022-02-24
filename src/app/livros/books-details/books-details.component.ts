c03556c1c122a577507ac2d92c2eb0d934544281
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2022-02-24 17:21:05 -0300

    2 files added, 2 deleted

--- src/app/livros/books-details/books-details.component.ts
+++ src/app/livros/books-details/books-details.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-livros-details',
-  templateUrl: './livros-details.component.html',
+  selector: 'app-books-details',
+  templateUrl: './books-details.component.html',
 })
 export class LivrosDetailsComponent implements OnInit {
-  public nomeBotao = 'Voltar';
-  public typeBotao = 'button';
+  public name = 'Voltar';
+  public typeButton = 'button';
 
   constructor() {}
 
